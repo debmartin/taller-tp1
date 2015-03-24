c72e09cbb1f64b8074ae4b524e738ae99557ffe2
valeria.mrb <valeria.mrb@gmail.com>
2015-03-24 01:05:02 +0000

    Agregué sprites de Sub-Zero

--- src/controlador/Controlador.cpp
+++ src/controlador/Controlador.cpp
@@ -7,12 +7,20 @@
 #include "Controlador.h"
 
 void Controlador::manejar_Evento(SDL_Event &evento){
-	//Si se presiono una tecla
+	//Si se presiona una tecla
 	if( evento.type == SDL_KEYDOWN && evento.key.repeat == 0 ){
-		switch( evento.key.keysym.sym )
-		{
+		switch( evento.key.keysym.sym ){
 			case SDLK_LEFT: this->unPersonaje.caminarIzquierda(); break;
 		 	case SDLK_RIGHT: this->unPersonaje.caminarDerecha(); break;
+			case SDLK_UP: this->unPersonaje.moverArriba(); break;
+			case SDLK_DOWN: this->unPersonaje.moverAbajo(); break;
+		}
+	}
+	//Si se suelta una tecla
+	if( evento.type == SDL_KEYDOWN && evento.key.repeat == 0 ){
+		switch( evento.key.keysym.sym ){
+			case SDLK_LEFT: this->unPersonaje.caminarIzquierda(); break;
+			case SDLK_RIGHT: this->unPersonaje.caminarDerecha(); break;
 			case SDLK_UP: this->unPersonaje.moverArriba(); break;
 			case SDLK_DOWN: this->unPersonaje.moverAbajo(); break;
 		}
