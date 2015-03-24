d606562b7eb10ccb493ed20676c0868cf555d8c6
gastonquispe@gmail.com <gastonquispe@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-03-24 17:22:32 +0000

    Clase sprite creada.

---
+++ trunk/src/vista/Sprite.cpp
@@ -0,0 +1,18 @@
+/*
+ * Sprite.cpp
+ *
+ *  Created on: 23/3/2015
+ *      Author: gaston
+ */
+
+#include "Sprite.h"
+
+Sprite::Sprite() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Sprite::~Sprite() {
+	// TODO Auto-generated destructor stub
+}
+
