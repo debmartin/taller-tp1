4a8bd7e06f0dcca1a3b7a05b2e77776ce6c9eeb6
arielcorreofiuba@gmail.com <arielcorreofiuba@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-03-24 14:20:49 +0000

    Se agrega test del parser usando jsoncpp. se cambia la posicion de main.cpp

---
+++ trunk/test/parser/ParserTests.cpp
@@ -0,0 +1,25 @@
+/*
+ * ParserTests.cpp
+ *
+ *  Created on: Mar 24, 2015
+ *      Author: ariel
+ */
+
+#include "ParserTests.h"
+#include <iostream>
+using namespace std;
+
+ParserTests::ParserTests() {
+	// TODO Auto-generated constructor stub
+
+}
+
+void ParserTests::ejecutar() {
+
+	cout<<"Iniciar test del parser(jsoncpp) ..."<<endl;
+}
+
+ParserTests::~ParserTests() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ trunk/test/parser/ParserTests.h
@@ -0,0 +1,18 @@
+/*
+ * ParserTests.h
+ *
+ *  Created on: Mar 24, 2015
+ *      Author: ariel
+ */
+
+#ifndef TEST_PARSER_PARSERTESTS_H_
+#define TEST_PARSER_PARSERTESTS_H_
+
+class ParserTests {
+public:
+	ParserTests();
+	void ejecutar();
+	virtual ~ParserTests();
+};
+
+#endif /* TEST_PARSER_PARSERTESTS_H_ */
