f6dbdeffd679ad23d42b10ff428a536829fe6547
gastonquispe@gmail.com <gastonquispe@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-03-24 23:37:00 +0000

    Agrego clase a Vista.

---
+++ trunk/src/vista/EscenarioGrafico.cpp
@@ -0,0 +1,18 @@
+/*
+ * EscenarioGrafico.cpp
+ *
+ *  Created on: 24/3/2015
+ *      Author: gaston
+ */
+
+#include "EscenarioGrafico.h"
+
+EscenarioGrafico::EscenarioGrafico() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EscenarioGrafico::~EscenarioGrafico() {
+	// TODO Auto-generated destructor stub
+}
+
