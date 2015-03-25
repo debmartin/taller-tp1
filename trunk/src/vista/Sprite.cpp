9fef4545eafd45a1743a979f416a8443d983d80e
gastonquispe@gmail.com <gastonquispe@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-03-25 19:33:54 +0000

    Modificado clase Sprite

--- trunk/src/vista/Sprite.cpp
+++ trunk/src/vista/Sprite.cpp
@@ -22,7 +22,7 @@ Sprite::Sprite(string id_textura, Vector2 posicion, int cantidadFotogramas)
 }
 
 void Sprite::dibujar(){
-/*
+
 	SDL_Rect srcRect;
 	srcRect.x = (fotogramaActual - 1) * this->anchoFotogramaPx;
 	srcRect.y = 0;
@@ -30,12 +30,28 @@ void Sprite::dibujar(){
 	srcRect.h = altoPx;
 
 	SDL_Rect destRect;
-	srcRect.x = 0; //
-	srcRect.y = 0;
+	srcRect.x = posicion.getCoordenadaX();
+	srcRect.y = posicion.getCoordenadaY();
 	srcRect.w = anchoFotogramaPx;
 	srcRect.h = altoPx;
-*/
-	SDL_RenderCopyEx(Game::Instance()->getRenderer(),GestorTexturas::Instance()->getTextura(id_textura), NULL, NULL, 0, NULL, SDL_FLIP_NONE);
+
+	SDL_RenderCopyEx(Game::Instance()->getRenderer(),GestorTexturas::Instance()->getTextura(id_textura),&srcRect,&destRect,	0,NULL,SDL_FLIP_NONE);
 }
 
+void Sprite::setPosicion(Vector2 nuevaPosicion) {
+	posicion = nuevaPosicion;
+}
+
+void Sprite::setFlip(SDL_RendererFlip f) {
+	this->flip = f;
+}
+
+void Sprite::avanzarFotograma() {
+	if (fotogramaActual < cantidadFotogramas)
+		fotogramaActual++;
+	else
+		fotogramaActual = 1;
+}
+
+//SDL_Rect* getAreaDibujable();
 Sprite::~Sprite() {}
