58aba4a8b6c8f3c823ca148520a77159bc8ee4c8
valeria.mrb@gmail.com <valeria.mrb@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-03-26 22:32:45 +0000

    Modificación de VentanaGráfica

--- trunk/src/vista/VentanaGrafica.h
+++ trunk/src/vista/VentanaGrafica.h
@@ -1,20 +1,28 @@
 #ifndef SRC_VISTA_VENTANAGRAFICA_H_
 #define SRC_VISTA_VENTANAGRAFICA_H_
 
+#include <SDL2/SDL.h>
+
 using namespace std;
 
 class VentanaGrafica {
 private:
+	//SRL_Renderer* vRenderer;
+	SDL_Window* vWindow;
+
+	// SINGLETON
+	VentanaGrafica();
+	static VentanaGrafica* instancia_unica_ventana;
 
 public:
-	VentanaGrafica();
 	virtual ~VentanaGrafica();
-
-	void agregarSprite(Sprite* s, string s);
+	void init(string titulo, SDL_Rect disposicion, bool fullscreen);
+	SDL_Renderer* getRenderer();
+	void agregarSprite(Sprite* unSprite, string s);
 	void quitarSprite(string id);
 	void cambiarSprite(string id, Sprite* nuevoSprite);
 	void getSprite(string id);
-	void dibujar();
+	void dibujarTodo();
 };
 
 #endif /* SRC_VISTA_VENTANAGRAFICA_H_ */
