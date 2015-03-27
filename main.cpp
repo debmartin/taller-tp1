106d00164f0494ff6d87f82d6e338e5ef1a6a3fe
gastonquispe <gastonquispe@gmail.com>
2015-03-27 09:50:55 +0000

    Modificacion para reproducir con un fps fijo.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,39 @@
 #include "src/Game.h"
 #include "test/Test.h"
+#include <SDL2/SDL.h>
 
 int main(int argc, char* args[])
 {	
+	static const int FPS = 60;
+	static const int DELAY_TIME = 1000.0f / FPS;
+
+	Uint32 frameStart, frameTime;
+
 	//para correr las pruebas comentar o descomentar estas 2 lineas
 	Test tests;
 	tests.ejecutar();
 
-	Game* g_game = NULL;
-
-	g_game = Game::Instance();
+	Game g_game("Taller de programacion TP", 100, 100, 640, 480, false);
 
 	//levantar la información inicial mediante json
 	//... usar libreria jsonCpp
 
-	g_game->init("Taller de programacion TP", 100, 100, 640, 480, false);
 
-	while (g_game->running())
+	while (g_game.running())
 	{
-		g_game->handleEvents();
-		g_game->update();
-		g_game->render();
+		frameStart = SDL_GetTicks();
+
+		g_game.handleEvents();
+		g_game.update();
+		g_game.render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameTime < DELAY_TIME)
+			SDL_Delay((int)(DELAY_TIME - frameTime));
 	}
 
-	g_game->clean();
+	g_game.clean();
 
 	return 0;
 }
