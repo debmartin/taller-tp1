a36f80ecdac9e6375685e1be3f44b3ba12bd2936
valeria.mrb@gmail.com <valeria.mrb@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-03-27 20:00:44 +0000

    Subo la clase PersonajeDibujable, aviso que no va a compilar por unos segundos porque estamos cambiando cosas con Gastón.

--- trunk/src/vista/PersonajeDibujable.cpp
+++ trunk/src/vista/PersonajeDibujable.cpp
@@ -18,20 +18,20 @@ void PersonajeDibujable::setEstado(int unEstado){
 void PersonajeDibujable::seleccionarSprite(){
 	switch(this->estado){
 		case caminando_derecha:
-			//this->sprites.at(caminando_derecha).setSentidoReproduccion(derecha);
-			//VentanaGrafica::Instance()->agregarSprite(this->sprites[caminando_derecha]);
+			this->sprites.at(caminando_derecha)->setSentidoReproduccion(HACIA_ADELANTE);
+			VentanaGrafica::Instance()->agregarSprite(this->sprites[caminando_derecha]);
 			break;
 		case caminando_izquierda:
-			//this->sprites[caminando_izquierda].setSentidoReproduccion(izquierda);
-			//ventana_grafica.agregarSprite(this->sprites[caminando_izquierda]);
+			this->sprites[caminando_izquierda]->setSentidoReproduccion(HACIA_ATRAS);
+			VentanaGrafica::Instance()->agregarSprite(this->sprites[caminando_izquierda]);
 			break;
 		case en_espera:
-			//this->sprites[en_espera].setSentidoReproduccion(derecha);
-			//ventana_grafica.agregarSprite(this->sprites[en_espera]);
+			this->sprites[en_espera]->setSentidoReproduccion(HACIA_ADELANTE));
+			VentanaGrafica::Instance()->agregarSprite(this->sprites[en_espera]);
 			break;
 		case saltando:
-			//this->sprites[saltando].setSentidoReproduccion(derecha);
-			//ventana_grafica.agregarSprite(this->sprites[saltando]);
+			this->sprites[saltando]->setSentidoReproduccion(HACIA_ADELANTE);
+			VentanaGrafica::Instance()->agregarSprite(this->sprites[saltando]);
 			break;
 	}
 }
--- trunk/src/vista/PersonajeDibujable.h
+++ trunk/src/vista/PersonajeDibujable.h
@@ -17,7 +17,6 @@
 using namespace std;
 
 enum estado{en_espera, caminando_derecha, caminando_izquierda, saltando};
-//enum SentidoReproduccion{derecha, izquierda};
 
 class PersonajeDibujable{
 private:
