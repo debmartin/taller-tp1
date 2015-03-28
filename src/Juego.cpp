eac056a11240b05a4e2c2705e0e633433514c097
gastonquispe <gastonquispe@gmail.com>
2015-03-28 23:21:08 +0000

    Ya funciona el z-index. Al momento de crear un sprite se le debe asginar el z-index y listo. Ventana Grafica se encarga de dibujarTodo en orden. Los numeros menores van al fondo, los mayores hacia adelante.

--- src/Juego.cpp
+++ src/Juego.cpp
@@ -47,9 +47,9 @@ Juego::Juego(const char* titulo, int xpos, int ypos, int altura, int ancho, bool
 
 	// 2) Cargar Sprites
 	Sprite* sprPit;
-	sprPit = new Sprite("screen-pit", Vector2(0, 0), 1, 1, 1);
+	sprPit = new Sprite("screen-pit", Vector2(0, 0), 1, 0, 1);
 	sprPit->escalarConTamanio(640,480);
-	//VentanaGrafica::Instance()->setSprite(sprPit, "sprPit");
+	VentanaGrafica::Instance()->setSprite(sprPit, "sprPit");
 
 	sprSubZeroQuieto = new Sprite("zubzero-quieto", Vector2(0, 0), 12, 1, 10);
 	sprSubZeroQuieto->setPosicion(Vector2(80,195));
