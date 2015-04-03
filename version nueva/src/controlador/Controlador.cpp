86e8e9e4a38706e0f0cea42a4236f46f91ad3e70
debbie1new.world <debbie1new.world@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-04-03 20:27:48 +0000

    adapto controlador para que reciba jugador

--- version nueva/src/controlador/Controlador.cpp
+++ version nueva/src/controlador/Controlador.cpp
@@ -6,7 +6,8 @@
  */
 #include "Controlador.h"
 
-Controlador::Controlador(){
+Controlador::Controlador(Personaje* pers) :
+    unPersonaje(pers) {
 }
 
 void Controlador::manejar_Evento(SDL_Event &evento){
@@ -14,19 +15,19 @@ void Controlador::manejar_Evento(SDL_Event &evento){
 	if( evento.type == SDL_KEYDOWN && evento.key.repeat == 0 ){
 		switch( evento.key.keysym.sym ){
 			case SDLK_LEFT:
-				this->unPersonaje.disminuir_velocidadX();
+				this->unPersonaje->disminuir_velocidadX();
 //				this->unPersonajeDibujable.setEstado(CAMINANDO_IZQUIERDA);
 				break;
 		 	case SDLK_RIGHT:
-		 		this->unPersonaje.aumentar_velocidadX();
+		 		this->unPersonaje->aumentar_velocidadX();
 //		 		this->unPersonajeDibujable.setEstado(CAMINANDO_DERECHA);
 		 		break;
 			case SDLK_UP:
-				this->unPersonaje.aumentar_velocidadY();
+				this->unPersonaje->aumentar_velocidadY();
 //				this->unPersonajeDibujable.setEstado(SALTANDO);
 				break;
 			case SDLK_DOWN:
-				this->unPersonaje.disminuir_velocidadY();
+				this->unPersonaje->disminuir_velocidadY();
 //				this->unPersonajeDibujable.setEstado(SALTANDO);
 				break;
 		}
@@ -35,26 +36,26 @@ void Controlador::manejar_Evento(SDL_Event &evento){
 	if( evento.type == SDL_KEYUP && evento.key.repeat == 0 ){
 		switch( evento.key.keysym.sym ){
 			case SDLK_LEFT:
-				this->unPersonaje.aumentar_velocidadX();
+				this->unPersonaje->aumentar_velocidadX();
 //				this->unPersonajeDibujable.setEstado(EN_ESPERA);
 				break;
 			case SDLK_RIGHT:
-				this->unPersonaje.disminuir_velocidadX();
+				this->unPersonaje->disminuir_velocidadX();
 //				this->unPersonajeDibujable.setEstado(EN_ESPERA);
 				break;
 			case SDLK_UP:
-				this->unPersonaje.disminuir_velocidadY();
+				this->unPersonaje->disminuir_velocidadY();
 //				this->unPersonajeDibujable.setEstado(EN_ESPERA);
 				break;
 			case SDLK_DOWN:
-				this->unPersonaje.aumentar_velocidadY();
+				this->unPersonaje->aumentar_velocidadY();
 //				this->unPersonajeDibujable.setEstado(EN_ESPERA);
 				break;
 		}
 	}
 }
 
-void Controlador::setPersonaje(Personaje personaje){
+void Controlador::setPersonaje(Personaje* personaje){
 	this->unPersonaje = personaje;
 }
 
