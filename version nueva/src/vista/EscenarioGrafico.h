7791704476b25f00cada42ab03699ffe9c2a01a5
valeria.mrb <valeria.mrb@gmail.com>
2015-04-04 14:10:49 +0000

    Agregado de LimitesLogicos, interfaz Posicionable y cambios en EscenarioGrafico.

--- version nueva/src/vista/EscenarioGrafico.h
+++ version nueva/src/vista/EscenarioGrafico.h
@@ -8,14 +8,17 @@
 class EscenarioGrafico : public Dibujable {
 private:
 	std::list<Dibujable*>* dibujables;
-	double ancho;
-	double alto;
+	double ancho_logico;
+	double alto_logico;
+	double y_piso_logico;
+
 
 public:
 	EscenarioGrafico(double ancho, double alto, std::list<Dibujable*>* dibujables);
 	virtual ~EscenarioGrafico();
-	double getAlto() const;
-	double getAncho() const;
+	double getAltoLogico() const;
+	double getAnchoLogico() const;
+	double getYPisoLogico() const;
 	void dibujar();
 	void actualizar();
 };
