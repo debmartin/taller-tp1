8b5df80b244a28768334dded2579d129c0edb427
valeria.mrb <valeria.mrb@gmail.com>
2015-04-09 17:57:36 +0000

    Agregue una lista de punteros a capas para manejar el scroll

--- version nueva/src/vista/EscenarioGrafico.cpp
+++ version nueva/src/vista/EscenarioGrafico.cpp
@@ -2,8 +2,8 @@
 
 using std::list;
 
-EscenarioGrafico::EscenarioGrafico(double ancho, double alto, double y_piso, list<Dibujable*>* dibujables) :
-    ancho_logico(ancho), alto_logico(alto), capas(dibujables) {
+EscenarioGrafico::EscenarioGrafico(double ancho, double alto, double y_piso, list<Dibujable*>* dibujables, list<Capa*>* capas) :
+    ancho_logico(ancho), alto_logico(alto), dibujables(dibujables), capas(capas) {
 }
 
 double EscenarioGrafico::getAltoLogico() const {
@@ -24,24 +24,28 @@ EscenarioGrafico::~EscenarioGrafico() {
 }
 
 void EscenarioGrafico::dibujar(){
-    for (list<Dibujable*>::iterator it = capas->begin(); it != capas->end(); ++it){
+    for (list<Dibujable*>::iterator it = dibujables->begin(); it != dibujables->end(); ++it){
         (*it)->dibujar();
     }
 }
 
 void EscenarioGrafico::actualizar(){
-    for (list<Dibujable*>::iterator it = capas->begin(); it != capas->end(); ++it){
+    for (list<Dibujable*>::iterator it = dibujables->begin(); it != dibujables->end(); ++it){
         (*it)->actualizar();
     }
 }
 
 void EscenarioGrafico::scrollear_capas(){
-	Dibujable* dib = this->capas->front();
+/*	Dibujable* dib = this->capas->front();
 	Capa* capa = (Capa*) dib;
-	Sprite* sprite = capa->getSprite();
-	Vector2f v(VentanaGrafica::Instance()->getLimiteLogicoIzquierdo() * VentanaGrafica::Instance()->relacion_de_aspectoX(), 0.0f);
-	v*=-1;
-	sprite->setPosicion(v);
+	Sprite* sprite = capa->getSprite();*/
+
+	for (list<Capa*>::iterator it = capas->begin(); it != capas->end(); ++it){
+		Vector2f v(VentanaGrafica::Instance()->getLimiteLogicoIzquierdo() * VentanaGrafica::Instance()->relacion_de_aspectoX(), 0.0f);
+		v*=-1;
+		Sprite* sprite = (*it)->getSprite();
+	    sprite->setPosicion(v);
+	}
 }
 
 bool EscenarioGrafico::esValida(Vector2f posicion){
