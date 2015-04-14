7746149e57429da18a5eaade20c1bfca428b176b
valeria.mrb@gmail.com <valeria.mrb@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-04-14 20:08:58 +0000

    Agrego el Cargador de Objetos con la inicialización desde Parser. Faltan los id de fondos y sprites para poder usarlo por eso lo dejo entre comentarios.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,5 +1,6 @@
 #include "src/Juego.h"
 #include "test/Test.h"
+/*
 #include "src/json/Parser.h"
 #include "src/utils/Logger.h"
 #include "src/json/EscenarioDef.h"
@@ -12,7 +13,8 @@
 #include "src/modelo/Personaje.h"
 #include "src/vista/Sprite.h"
 #include <jsoncpp/json/json.h>
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>*/
+#include "src/modelo/CargadorDeObjetos.h"
 
 #define TITULO_VENTANA "Taller de programacion TP: Mortal Kombat"
 #define POS_VENTANA_INICIO Vector2f(100, 100)
@@ -79,140 +81,39 @@ int main(int argc, char* args[])
 
 	Uint32 frameStart, frameTime;
 
-//////////Parser/////////
-	//levantar la información inicial mediante json
-	//... usar libreria jsonCpp
-/*	string nombre_escenario("src/recursos/escenario.json");
+	////Inicializacion del Cargador de Objetos////
+/*	CargadorDeOjbetos cargador_de_objetos;
+	cargador_de_objetos.cargarObjetos();
 
-	Parser* parser = new Parser(nombre_escenario.c_str());
+	EscenarioGrafico escenario = *(cargador_de_objetos.getEscenarioGrafico());
+	Personaje jugador = *(cargador_de_objetos.getPersonaje());
+	PersonajeDibujable personajeDibujable = *(cargador_de_objetos.getPersonajeDibujable());
 
-	parser->parsearDesdeJson();
+	Juego g_game(VentanaGrafica::Instance(), &escenario, &jugador, &personajeDibujable);
 
-	VentanaDef* ventanaDef = parser->getVentanaDef();
-	cout<<*ventanaDef<<endl;
+	    cout << "creacion juego correcta" << endl;
 
-	EscenarioDef* escenarioDef = parser->getEscenarioDef();
-	cout<<*escenarioDef<<endl;
+		while (g_game.running())
+		{
 
-	PersonajeDef* personajeDef = parser->getPersonajeDef();
-	cout<<*personajeDef<<endl;
+			frameStart = SDL_GetTicks();
 
-	//para correr las pruebas comentar o descomentar estas 2 lineas
-//	Test tests;
-//	tests.ejecutar();
-    Vector2f tamanioVentanaPx(ventanaDef->getAnchoPx(), ventanaDef->getAltoPx());
-    Vector2f tamanioVentanaLogico(ventanaDef->getAncho(), escenarioDef->getAlto());
-    bool exito = VentanaGrafica::Instance()->init(TITULO_VENTANA, POS_VENTANA_INICIO, tamanioVentanaPx, tamanioVentanaLogico, INICIAR_FULLSCREEN);
+			g_game.handleEvents();
 
-    if (! exito)
-    	Logger::getInstance()->error("Error al inicializar juego");
+			g_game.update();
+		    //cout << "game running" << endl;
 
-    Logger::getInstance()->info("Inicialiazación de VentanaGrafica correcta.");
+			g_game.render();
+		    //cout << "game running2" << endl;
 
+			frameTime = SDL_GetTicks() - frameStart;
 
-	Vector2f relacionAspectos = VentanaGrafica::Instance()->obtener_relacion_aspectos();
-
-	list<Capa*> capas;
-	list<Dibujable*> capasYPersonajes;
-	for (list<CapaDef*>::iterator it_capas = parser->getCapasDef()->begin() ; it_capas != parser->getCapasDef()->end(); it_capas++)
-	{
-		cout<<**it_capas<<endl;
-
-		Animacion fondoAnim((*it_capas)->getImagenFondo(), CANT_FOTOGRAMAS_FONDO, FPS_FONDO, ID_FONDO, Renderizador::Instance()->getRenderer());
+			if (frameTime < DELAY_TIME)
+				SDL_Delay((int)(DELAY_TIME - frameTime));
+		}
 
 
-
-		Vector2f tamIniCapa((*it_capas)->getAncho(), escenarioDef->getAlto());
-		Vector2f posInCapa = POSICION_INICIAL_CAPA;
-
-		Capa fondoCapa(&fondoAnim, tamIniCapa, posInCapa, relacionAspectos);
-		capas.push_back(&fondoCapa);
-		capasYPersonajes.push_back((Dibujable*) &fondoCapa);
-
-	}
-
-	Animacion zubQuieto(IMAGEN_ZUBZERO_QUIETO, CANT_FOTOGRAMAS_ZUBQUIETO, FPS_ZUBQUIETO,  ID_ZUBZERO_QUIETO, Renderizador::Instance()->getRenderer());
-	Animacion zubCaminando(IMAGEN_ZUBZERO_CAMINANDO, CANT_FOTOGRAMAS_ZUBCAMINANDO, FPS_ZUBCAMINANDO, ID_ZUBZERO_CAMINANDO, Renderizador::Instance()->getRenderer());
-
-	Personaje jugador(personajeDef->getAncho(), personajeDef->getAlto(), POSICION_INICIAL_PERSONAJE, VentanaGrafica::Instance());
-	Vector2f tamanioPx(
-    		jugador.getAncho()*VentanaGrafica::Instance()->relacion_de_aspectoX(),
-			jugador.getAlto()*VentanaGrafica::Instance()->relacion_de_aspectoY());
-	PersonajeDibujable personaje(&zubQuieto, POSICION_INICIAL_PERSONAJE, tamanioPx);
-	personaje.agregarAnimacion(&zubQuieto);
-	personaje.agregarAnimacion(&zubCaminando);
-
-	capasYPersonajes.push_back((Dibujable*) &personaje);
-
-
-
-	EscenarioGrafico escenario(escenarioDef->getAncho(), escenarioDef->getAlto(), escenarioDef->getYpiso(), &capasYPersonajes, &capas);
-    Logger::getInstance()->info("Inicializacion de EscenarioGrafico correcta.");
-
-
-	VentanaGrafica::Instance()->agregarEscenario(&escenario);
-    VentanaGrafica::Instance()->centrar_ventana();////////////////////////////////////////////////////////////////////////////////////
-    escenario.centrar_dibujables();////////////////////////////////////////////////////////////////////////////////////////////////////////
-    Vector2f vector_centrado(escenarioDef->getAncho()/2.0, escenarioDef->getYpiso());
-    jugador.centrar_en(vector_centrado);////////////////////////////////////////////////////////////////////////////////
-
-    Logger::getInstance()->info("Carga correcta.");
-    cout << "carga correcta" << endl;
-	Juego g_game(VentanaGrafica::Instance(), &escenario, &jugador, &personaje);
-	Logger::getInstance()->info("Creación de juego correcta.");
-    cout << "creacion juego correcta" << endl;
-
-
-
-	while (g_game.running())
-	{
-
-		frameStart = SDL_GetTicks();
-
-		g_game.handleEvents();
-
-		g_game.update();
-	    //cout << "game running" << endl;
-
-		g_game.render();
-	    //cout << "game running2" << endl;
-		cout << "============INICIO INFORMACION DE OBJETOS============" << endl;
-		cout << "ESCENARIO->alto-logico:" << escenario.getAltoLogico() << endl;
-		cout << "ESCENARIO->ancho-logico:" << escenario.getAnchoLogico() << endl;
-		cout << "ESCENARIO->y-piso-logico:" << escenario.getYPisoLogico() << endl;
-		cout << "-------------------------------------------------------" << endl;
-		cout << "VENTANA-GRAFICA->limite logico Izquierdo:" << VentanaGrafica::Instance()->getLimiteLogicoIzquierdo() << endl;
-		cout << "VENTANA-GRAFICA->limite logico Derecho:" << VentanaGrafica::Instance()->getLimiteLogicoDerecho() << endl;
-		cout << "VENTANA-GRAFICA->relacion de aspecto X:" << VentanaGrafica::Instance()->relacion_de_aspectoX() << endl;
-		cout << "VENTANA-GRAFICA->relacion de aspecto Y:" << VentanaGrafica::Instance()->relacion_de_aspectoY() << endl;
-		cout << "-------------------------------------------------------" << endl;
-		cout << "CAPA1->limite logico Izquierdo:" << fondoCapa.getLimiteLogicoIzquierdo() << endl;
-		cout << "CAPA1->limite logico Derecho:" << fondoCapa.getLimiteLogicoDerecho() << endl;
-		cout << "CAPA1->sprite->altpPx:" << fondoCapa.getSprite()->getAltoPx() << endl;
-		cout << "CAPA1->sprite->anchoPx:" << fondoCapa.getSprite()->getAnchoPx() << endl;
-		cout << "CAPA1->sprite->posicionPx-X:" << fondoCapa.getSprite()->getPosicion().X() << endl;
-		cout << "CAPA1->sprite->posicionPx-Y:" << fondoCapa.getSprite()->getPosicion().Y() << endl;
-		cout << "-------------------------------------------------------" << endl;
-		cout << "CAPA2->limite logico Izquierdo:" << fondoCapa2.getLimiteLogicoIzquierdo() << endl;
-		cout << "CAPA2->limite logico Derecho:" << fondoCapa2.getLimiteLogicoDerecho() << endl;
-		cout << "CAPA2->sprite->altpPx:" << fondoCapa2.getSprite()->getAltoPx() << endl;
-		cout << "CAPA2->sprite->anchoPx:" << fondoCapa2.getSprite()->getAnchoPx() << endl;
-		cout << "CAPA2->sprite->posicionPx-X:" << fondoCapa2.getSprite()->getPosicion().X() << endl;
-		cout << "CAPA2->sprite->posicionPx-Y:" << fondoCapa2.getSprite()->getPosicion().Y() << endl;
-		cout << "-------------------------------------------------------" << endl;
-		cout << "PERSONAJE-LOGICO->posicion-logica-X:" << jugador.getPosicion().X() << endl;
-		cout << "PERSONAJE-LOGICO->posicion-logica-Y:" << jugador.getPosicion().Y() << endl;
-		cout << "-------------------------------------------------------" << endl;
-		cout << "PERSONAJE-DIBUJABLE->SPRITE->posicionPx-X:" << personaje._getSprite()->getPosicion().X() << endl;
-		cout << "PERSONAJE-DIBUJABLE->SPRITE->posicionPx-Y:" << personaje._getSprite()->getPosicion().Y() << endl;
-		cout << "=============FIN INFORMACION DE OBJETOS==============" << endl;
-		frameTime = SDL_GetTicks() - frameStart;
-
-		if (frameTime < DELAY_TIME)
-			SDL_Delay((int)(DELAY_TIME - frameTime));
-	}
-
-////////Fin//////////
+	////Fin de inicializacion desde Parser////
 */
 	//para correr las pruebas comentar o descomentar estas 2 lineas
 	//Test tests;
