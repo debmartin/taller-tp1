7ae4b51359c3a801c6d112b537a4bbc4435cd2f2
gastonquispe@gmail.com <gastonquispe@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-04-15 02:08:10 +0000

    Hecho el loggeo para Sprite.

--- trunk/src/vista/Sprite.h
+++ trunk/src/vista/Sprite.h
@@ -2,8 +2,11 @@
 #ifndef SRC_VISTA_SPRITE_H_
 #define SRC_VISTA_SPRITE_H_
 #include <SDL2/SDL.h>
-#include "../../src/modelo/Vector2f.h"
+#include <iostream>
 #include <string>
+#include <math.h>
+#include "../utils/Logger.h"
+#include "../modelo/Vector2f.h"
 #include "../modelo/Trayectoria.h"
 #include "../modelo/Reposo.h"
 #include "Animacion.h"
@@ -33,11 +36,9 @@ private:
 	float factorEscalaY;
 	int anchoFotogramaPx;
 	int fotogramaActual;
-//	float zIndex;
 	int fps;
-	SDL_RendererFlip flip;
+	OrientacionSprite orientacion;
 	SentidoReproduccion sentidoReproduccion;
-	float tCreacion;
 
 public:
 	Sprite(Animacion* animInicial, Vector2f& posicion, OrientacionSprite orientacion);
@@ -45,13 +46,12 @@ public:
 
 	void setPosicion(Vector2f& posicion);
 	void desplazar(Vector2f& posicion);
-	void setFlip(SDL_RendererFlip f); //OK
-	//void avanzarFotograma();
-	void escalarConFactor(Vector2f& factor); //OK
-	void escalarConTamanio(int anchoNuevoPx, int altoNuevoPx); //OK
+	void setOrientacion(OrientacionSprite o);
+	void escalarConFactor(Vector2f& factor);
+	void escalarConTamanio(int anchoNuevoPx, int altoNuevoPx);
 	void setFotogramaActual(int nroFotograma);
 	void setSentidoReproduccion(SentidoReproduccion sr);
-	void dibujar(); // OK
+	void dibujar();
 	void update();
 	void cambiarAnimacion(Animacion* nuevaAnim);
 	Vector2f getPosicion();
