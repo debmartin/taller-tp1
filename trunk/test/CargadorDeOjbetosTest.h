0e5e0562696fbee8e139bf605daffff279602474
arielcorreofiuba <arielcorreofiuba@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-04-15 01:58:49 +0000

    se agrego un test para el cargador de Objetos y asi poder arreglar ese bug que no terminar de cargar los objetos

---
+++ trunk/test/CargadorDeOjbetosTest.h
@@ -0,0 +1,18 @@
+/*
+ * CargadorDeOjbetosTest.h
+ *
+ *  Created on: 14/4/2015
+ *      Author: ariel
+ */
+
+#ifndef TEST_CARGADORDEOJBETOSTEST_H_
+#define TEST_CARGADORDEOJBETOSTEST_H_
+
+class CargadorDeOjbetosTest {
+public:
+	CargadorDeOjbetosTest();
+	void ejecutar();
+	virtual ~CargadorDeOjbetosTest();
+};
+
+#endif /* TEST_CARGADORDEOJBETOSTEST_H_ */
