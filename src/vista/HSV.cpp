822fcad25a95fc3b6ef461ea8c40c77f392453ca
valeria.mrb <valeria.mrb@gmail.com>
2015-04-25 23:12:44 +0000

    Transformaciones de HSV Y RGB

--- src/vista/HSV.cpp
+++ src/vista/HSV.cpp
@@ -16,14 +16,17 @@ HSV::HSV(SDL_Texture* s) {
 	float B;
 
 	//Hallo el máximo y el mínimo
-	max = maximo(R, G, B);
-	min = minimo(R, G, B);
+	maxRGB = maximo(R, G, B);
+	minRGB = minimo(R, G, B);
 
 	setH(R,G,B);
 	setS(R,G,B);
 	setV(R,G,B);
 
 	//Operar con los valores de HSV
+	transformar_HSV_a_RGB();
+
+	//Operar con los valores finales de RGB
 }
 
 int HSV::maximo(int R, int G, int B){
@@ -43,28 +46,63 @@ int HSV::minimo(int R, int G, int B){
 }
 
 void HSV::setH(int R, int G, int B){
-	if(max == min){
+	if(maxRGB == minRGB){
 		//no definido
-	}else if(max == R && G >= B){
-		H = GRADOS60 * (G - B)/(max - min) + GRADOS0;
-	}else if(max == R && G < B){
-		H = GRADOS60 * (G - B)/(max - min) + GRADOS360;
-	}else if(max == G){
-		H = GRADOS60 * (B - R)/(max - min) + GRADOS120;
-	}else if(max == B){
-		H = GRADOS60 * (R - G)/(max - min) + GRADOS120;
+	}else if(maxRGB == R && G >= B){
+		H = GRADOS60 * (G - B)/(maxRGB - minRGB) + GRADOS0;
+	}else if(maxRGB == R && G < B){
+		H = GRADOS60 * (G - B)/(maxRGB - minRGB) + GRADOS360;
+	}else if(maxRGB == G){
+		H = GRADOS60 * (B - R)/(maxRGB - minRGB) + GRADOS120;
+	}else if(maxRGB == B){
+		H = GRADOS60 * (R - G)/(maxRGB - minRGB) + GRADOS120;
 	}
 }
 
 void HSV::setS(int R, int G, int B){
-	if(max == 0){
+	if(maxRGB == 0){
 		S = 0;
 	}
-	S = (1-max/min);
+	S = (1-maxRGB/minRGB);
 }
 
 void HSV::setV(int R, int G, int B){
-	V = max;
+	V = maxRGB;
+}
+
+/*Precondición H <= 360*/
+void HSV::transformar_HSV_a_RGB(){
+	float Hinicial = (H/60)%6;
+	float f = ((H/60)%6) - Hinicial;
+	float p = V*(1-S);
+	float q = V*(1-f*S);
+	float t = V*(1-(1-f)*S);
+
+	if(Hinicial == 0){
+		Rfinal = V;
+		Gfinal = t;
+		Bfinal = p;
+	}else if(Hinicial == 1){
+		Rfinal = q;
+		Gfinal = V;
+		Bfinal = p;
+	}else if(Hinicial == 2){
+		Rfinal = p;
+		Gfinal = V;
+		Bfinal = t;
+	}else if(Hinicial == 3){
+		Rfinal = p;
+		Gfinal = q;
+		Bfinal = V;
+	}else if(Hinicial == 4){
+		Rfinal = t;
+		Gfinal = p;
+		Bfinal = V;
+	}else if(Hinicial == 5){
+		Rfinal = V;
+		Gfinal = p;
+		Bfinal = q;
+	}
 }
 
 HSV::~HSV() {
