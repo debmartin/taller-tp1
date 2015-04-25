fd5e60aa83ea6e10e850136b8964074f7a244fa9
gastonquispe@gmail.com <gastonquispe@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-04-25 14:14:37 +0000

    Agregada clase HSV para conversion de colores de Texturas.

---
+++ trunk/src/vista/HSV.cpp
@@ -0,0 +1,17 @@
+/*
+ * HSV.cpp
+ *
+ *  Created on: 25/4/2015
+ *      Author: gaston
+ */
+
+#include "HSV.h"
+
+HSV::HSV(SDL_Texture* s) {
+	// TODO Auto-generated constructor stub
+
+}
+
+HSV::~HSV() {
+	// TODO Auto-generated destructor stub
+}
