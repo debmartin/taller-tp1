8f5a26c7c1f5ea25e75018655f8f14a369b6c002
valeria.mrb@gmail.com <valeria.mrb@gmail.com@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-04-27 23:18:47 +0000

    Agregamos la firma de las primitivas que necesitamos que tengan las colisiones para que pueda funcionar todo acoplado a la vista sin que se rompa.

---
+++ trunk/src/modelo/fisica/BVH.cpp
@@ -0,0 +1,20 @@
+/*
+ * BVH.cpp
+ *
+ *  Created on: 27/4/2015
+ *      Author: val
+ */
+
+#include "BVH.h"
+
+BVH::BVH(vector <AABB>* aabb) {
+	// TODO Auto-generated constructor stub
+
+	//Crear la cajaLimitadora que envuelva todos los AABB dados.
+
+}
+
+BVH::~BVH() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ trunk/src/modelo/fisica/BVH.h
@@ -0,0 +1,32 @@
+/*
+ * BVH.h
+ *
+ *  Created on: 27/4/2015
+ *      Author: val
+ */
+
+#ifndef SRC_MODELO_FISICA_BVH_H_
+#define SRC_MODELO_FISICA_BVH_H_
+
+#include "AABB.h"
+#include <vector>
+using namespace std;
+
+class BVH {
+private:
+	AABB cajaLimitadora;
+	vector <AABB>* cajasAABB;
+	Vector2f desplazamiento;
+
+	void efectuarDesplazamiento();
+
+public:
+	BVH(vector <AABB>* aabb);
+	virtual ~BVH();
+	bool interseccion(BVH bvh);
+	void desplazarBVH(Vector2f v);
+	/*Espeja respecto al centro de la caja limitadora*/
+	void espejarBVH();
+};
+
+#endif /* SRC_MODELO_FISICA_BVH_H_ */
