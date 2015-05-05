33dbdfcc10c0f506fa877710cb9962815075b14c
arielcorreofiuba <arielcorreofiuba@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-05-05 06:19:53 +0000

    me falto subir la clase Jugador

---
+++ src/modelo/Jugador.cpp
@@ -0,0 +1,46 @@
+/*
+ * Jugador.cpp
+ *
+ *  Created on: 5/5/2015
+ *      Author: ariel
+ */
+
+#include "Jugador.h"
+
+Jugador::Jugador() {
+}
+
+Jugador::Jugador(Personaje* personaje, PersonajeDibujable* personajeDibujable, int direccion) {
+	this->personaje = personaje;
+	this->personajeDibujable = personajeDibujable;
+	this->direccion = direccion;
+}
+
+int Jugador::getDireccion() const {
+	return direccion;
+}
+
+void Jugador::setDireccion(int direccion) {
+	this->direccion = direccion;
+}
+
+Personaje* Jugador::getPersonaje() const {
+	return personaje;
+}
+
+void Jugador::setPersonaje(Personaje* personaje) {
+	this->personaje = personaje;
+}
+
+PersonajeDibujable* Jugador::getPersonajeDibujable() const {
+	return personajeDibujable;
+}
+
+void Jugador::setPersonajeDibujable(PersonajeDibujable* personajeDibujable) {
+	this->personajeDibujable = personajeDibujable;
+}
+
+Jugador::~Jugador() {
+	// TODO Auto-generated destructor stub
+}
+
