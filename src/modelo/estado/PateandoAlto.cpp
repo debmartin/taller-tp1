ce523e205a59e56bef3ae5665c9dca92b2677440
valeria.mrb <valeria.mrb@gmail.com>
2015-05-12 14:12:12 +0000

    Agrego los movimientos faltantes

--- src/modelo/estado/PateandoAlto.cpp
+++ src/modelo/estado/PateandoAlto.cpp
@@ -1,11 +1,18 @@
+/*
+ * PateandoAlto.cpp
+ *
+ *  Created on: 12/5/2015
+ *      Author: val
+ */
+
 #include "PateandoAlto.h"
 #include "../Reposo.h"
 
-#define DANIO_PATADA_ALTA 10
+#define DANIO_PATADA_BAJA 10
 
 PateandoAlto::PateandoAlto(Vector2f posInicial, BVH* caja) :
     Estado(new Reposo(posInicial), PATEANDO_ALTO, caja) {
-    ataqueEstado = new Ataque(DANIO_PATADA_ALTA);
+    ataqueEstado = new Ataque(DANIO_PATADA_BAJA);
 }
 
 PateandoAlto::~PateandoAlto()
@@ -17,6 +24,7 @@ bool PateandoAlto::estaAtacando(){
     return true;
 }
 
-bool PateandoAlto::ejecutandoMovimientoEspecial(){
-    return true;
+bool PateandoAlto::estaDefendiendo(){
+    return false;
 }
+
---
+++ src/modelo/estado/PateandoSaltandoVertical.cpp
@@ -0,0 +1,33 @@
+/*
+ * PateandoSaltandoVertical.cpp
+ *
+ *  Created on: 12/5/2015
+ *      Author: val
+ */
+
+#include "PateandoSaltandoVertical.h"
+#include "../Reposo.h"
+#include "../MRUV.h"
+
+#define DANIO_PATADA 10
+#define VELOCIDAD_DESP_VERTICAL 1120.0f
+#define VELOCIDAD_NULA 0.0f
+#define VECTOR_GRAVEDAD Vector2f(0, -2600.f)
+
+PateandoSaltandoVertical::PateandoSaltandoVertical(Vector2f posInicial, BVH* caja) :
+	Estado(new MRUV(posInicial, Vector2f(VELOCIDAD_NULA, VELOCIDAD_DESP_VERTICAL), VECTOR_GRAVEDAD), PATEANDO_SALTANDO_VERTICAL, caja) {
+	ataqueEstado = new Ataque(DANIO_PATADA);
+}
+
+PateandoSaltandoVertical::~PateandoSaltandoVertical()
+{
+//dtor
+}
+
+bool PateandoSaltandoVertical::estaAtacando(){
+	return true;
+}
+
+bool PateandoSaltandoVertical::estaDefendiendo(){
+	return false;
+}
