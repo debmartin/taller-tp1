4b4667bb64245a4bb8adfffaa50b7dcf3cc27e36
arielcorreofiuba <arielcorreofiuba@8e324fa4-feff-08c0-6224-aaff1bb7323d>
2015-06-14 18:19:27 +0000

    Se carga el personaje inteligente cuando corresponde

--- branches/taller-tp3/src/json/JugadorDef.h
+++ branches/taller-tp3/src/json/JugadorDef.h
@@ -19,6 +19,7 @@ using namespace std;
 
 class JugadorDef: public Loggeable {
 private:
+	string id;
 	string idPersonaje; // id del personaje al cual va a estar asociado
 	string tipo_control;
 	int tolerancia;
@@ -26,7 +27,7 @@ private:
 
 public:
 	JugadorDef();
-	JugadorDef(string tipo_control, int tolerancia, ControlDef* controlDef);
+	JugadorDef(string id, string tipo_control, int tolerancia, ControlDef* controlDef);
 	virtual ~JugadorDef();
 	friend ostream& operator<<(ostream &o, const JugadorDef &j);
 	string toString();
@@ -35,6 +36,7 @@ public:
 	int getTolerancia() const;
 	string getTipoControl() const;
 	void cargarIdPersonajeDesdeMenu(string nuevoIdPersonaje);
+	string getId() const;
 };
 
 #endif /* SRC_JSON_JUGADORDEF_H_ */
